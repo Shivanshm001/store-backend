4b116a4c19266f04242102d05a5ee8fd3505bca1
Shivansh Mishra <Shivanshmishra.web@gmail.com>
2023-09-20 15:48:03 +0530

    Added enum values for cateogry in product schema

--- src/database/models/Products.ts
+++ src/database/models/Products.ts
@@ -3,24 +3,24 @@ import { IProduct } from "../../interfaces/IProduct";
 
 
 interface ProductModel extends IProduct, Document { };
-// enum Catogries {
-//     Mobiles = "mobiles",
-//     Electronics = "electronics",
-//     Furniture = "furniture",
-//     Appliances = "appliances",
-//     Toys = "toys",
-//     Sports = "sports",
-//     Bikes = "bikes",
-//     Cars = "cars",
-//     Fashion = "fashion"
-// }
+enum Catogries {
+    Mobiles = "mobiles",
+    Electronics = "electronics",
+    Furniture = "furniture",
+    Appliances = "appliances",
+    Toys = "toys",
+    Sports = "sports",
+    Bikes = "bikes",
+    Cars = "cars",
+    Fashion = "fashion"
+}
 
 const productSchema = new Schema<ProductModel>({
     productID: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     imageName: { type: String, required: true },
     imageUrl: { type: String, required: true, default: " " },
-    category: { type: String, required: true, },
+    category: { type: String, required: true, enum: Object.values(Catogries) },
     company: { type: String, required: true },
     featured: { type: Boolean, required: true, default: false },
     rating: { type: Number, required: true, min: 1, max: 10 },
