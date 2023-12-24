301c20386655468eafb943142fe0d10869ebbc8c
Shivansh mishra <Shivanshmishra.chd@gmail.com>
2023-12-24 14:09:14 +0530

    Updated corsOrigin

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ import cookieParser from 'cookie-parser';
 
 const app: Express = express();
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
