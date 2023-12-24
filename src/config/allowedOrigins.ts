091125bcaeac02373f4dc25272db1fc3ab2e5cb3
Shivansh mishra <Shivanshmishra.chd@gmail.com>
2023-12-24 14:03:31 +0530

    Updated corsOrigin

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,5 +1,7 @@
 export const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:5173",
-    "http://127.0.0.1:5500"
+    "http://127.0.0.1:5500",
+    "https://store-backend-yylk.onrender.com/"
+
 ]
